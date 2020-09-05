38b99e7c70bf6b5e8976a8d5923069259f2e17dd
Damian Szczepański <dymber@gmail.com>
2020-09-05 09:19:22 +0200

    commit test

--- src/main/java/pl/sdacademy/main/Main.java
+++ src/main/java/pl/sdacademy/main/Main.java
@@ -2,6 +2,6 @@ package pl.sdacademy.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Wooow !!");
+        System.out.println("Wooow !!!");
     }
 }
